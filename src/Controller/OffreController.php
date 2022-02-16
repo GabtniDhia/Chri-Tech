d6043c6d92d9ea54b7199793e9376bfa4c3a9634
FaroukDamergi <fdamergi@gmail.com>
2022-02-16 23:48:17 +0100

    Update OffreController.php

--- src/Controller/OffreController.php
+++ src/Controller/OffreController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/offre")
+ * @Route("/offres")
  */
 class OffreController extends AbstractController
 {
