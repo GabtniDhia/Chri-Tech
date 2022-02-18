6dbf6d3defed2aea2b742d791e137bcf7469ccf4
GabtniDhia <74927920+GabtniDhia@users.noreply.github.com>
2022-02-19 00:04:42 +0100

    tiih

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -34,9 +34,6 @@ class RegistrationController extends AbstractController
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
-        if($form->isSubmitted()){
-            echo "<script>alert('YEEEEEES');</script>";
-        }
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
             $user->setPassword(
