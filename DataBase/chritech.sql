c67806afe603300d9fe2f098e80cf2459e824f94
GabtniDhia <74927920+GabtniDhia@users.noreply.github.com>
2022-02-23 15:05:03 +0100

    base (+message)

--- DataBase/chritech.sql
+++ DataBase/chritech.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : dim. 20 fév. 2022 à 13:53
+-- Généré le : mer. 23 fév. 2022 à 15:04
 -- Version du serveur : 10.4.21-MariaDB
 -- Version de PHP : 7.4.25
 
@@ -24,6 +24,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `article`
+--
+
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL,
+  `entete` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `corp` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` date NOT NULL,
+  `redacteur` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `avis`
 --
 
@@ -38,6 +53,20 @@ CREATE TABLE `avis` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `blog`
+--
+
+CREATE TABLE `blog` (
+  `id` int(11) NOT NULL,
+  `utilisateur` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `titre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contenue` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `date_heure` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `carte_fidelite`
 --
 
@@ -47,6 +76,18 @@ CREATE TABLE `carte_fidelite` (
   `date_creation` datetime NOT NULL,
   `date_expiration` datetime NOT NULL,
   `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id` int(11) NOT NULL,
+  `nom_cat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type_cat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -72,6 +113,21 @@ INSERT INTO `commande` (`id`, `nom`, `numtel`, `email`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `commentaire`
+--
+
+CREATE TABLE `commentaire` (
+  `id` int(11) NOT NULL,
+  `blog_id_id` int(11) DEFAULT NULL,
+  `article_id_id` int(11) DEFAULT NULL,
+  `utilisateur` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contenue` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_heure` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `doctrine_migration_versions`
 --
 
@@ -88,7 +144,8 @@ CREATE TABLE `doctrine_migration_versions` (
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
 ('DoctrineMigrations\\Version20220212091749', '2022-02-12 09:18:06', 173),
 ('DoctrineMigrations\\Version20220212092033', '2022-02-12 09:20:39', 21),
-('DoctrineMigrations\\Version20220212092745', '2022-02-12 09:27:51', 40);
+('DoctrineMigrations\\Version20220212092745', '2022-02-12 09:27:51', 40),
+('DoctrineMigrations\\Version20220222170824', '2022-02-22 18:08:28', 617);
 
 -- --------------------------------------------------------
 
@@ -103,6 +160,35 @@ CREATE TABLE `livraison` (
   `ville` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `datelivraison` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `recipient_id` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `message` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `is_read` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `messages`
+--
+
+INSERT INTO `messages` (`id`, `sender_id`, `recipient_id`, `title`, `message`, `created_at`, `is_read`) VALUES
+(1, 8, 5, 'aze', 'aze', '2022-02-22 19:01:25', 0),
+(2, 8, 5, 'Bonjour 123', 'AZE', '2022-02-22 19:34:58', 0),
+(3, 8, 5, 'Bonjour 123', 'AZE', '2022-02-22 19:35:06', 0),
+(4, 8, 5, 'Bonjour 123', 'AZE', '2022-02-22 19:35:39', 0),
+(5, 8, 5, 'Bonjour 123', 'AZE', '2022-02-22 19:36:13', 0),
+(6, 8, 10, 'Salem', 'TesT', '2022-02-22 20:31:51', 0),
+(7, 8, 10, 'Salem', 'TesT', '2022-02-22 20:32:26', 0);
 
 -- --------------------------------------------------------
 
@@ -137,13 +223,13 @@ CREATE TABLE `produit` (
   `id` int(11) NOT NULL,
   `ref_prod` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nom_prod` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `desc_prod` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `prix_unit_ht` double NOT NULL,
-  `qte_stock` int(11) NOT NULL,
-  `image_prod` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descri_prod` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image_prod` longblob DEFAULT NULL,
   `detail_prod` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `prix_ttc` double NOT NULL,
-  `prix_tva` int(11) NOT NULL
+  `prix_unit_ht_prod` double NOT NULL,
+  `qte_stock_prod` int(11) DEFAULT NULL,
+  `prix_ttc_prod` double NOT NULL,
+  `prix_tva_prod` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -174,22 +260,32 @@ CREATE TABLE `user` (
   `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   `roles` longtext COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '(DC2Type:json)',
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nom` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `prenom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `datecreation` date NOT NULL,
-  `is_verified` tinyint(1) NOT NULL
+  `is_verified` tinyint(1) NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `user`
 --
 
-INSERT INTO `user` (`id`, `email`, `roles`, `password`, `nom`, `prenom`, `datecreation`, `is_verified`) VALUES
-(5, 'admin@gmail.com', '[\"ROLE_ADMIN\",\"ROLE_CLIENT\",\"ROLE_SPECIALISTE\"]', '$argon2id$v=19$m=65536,t=4,p=1$bkt1eWhYc05JaG5sa3RNRA$peEcYw9WwQ4HGI4hPyzjC8DSr6VtMesNLF/hMZKK2GI', 'S9onslii', 'admin', '2017-01-01', 0);
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `nom`, `prenom`, `datecreation`, `is_verified`, `image`) VALUES
+(5, 'admin@gmail.com', '[\"ROLE_ADMIN\",\"ROLE_CLIENT\",\"ROLE_SPECIALISTE\"]', '$argon2id$v=19$m=65536,t=4,p=1$bkt1eWhYc05JaG5sa3RNRA$peEcYw9WwQ4HGI4hPyzjC8DSr6VtMesNLF/hMZKK2GI', 'S9onsliii', 'admin', '2017-01-01', 0, ''),
+(8, 'dhia.gabtni@esprit.tn', '[]', '$argon2id$v=19$m=65536,t=4,p=1$R0VIMkJVSXp1Qi9UU0NaZQ$01Dmm6le7CErGeBWIRZ/6fICjsRXx5O0jn1EAKi4QiY', 'Dhia', 'Gabtni', '2022-02-20', 1, '9105219d166997060ad6363aea8a90ee.png'),
+(9, 'Wehed@gmail.com', '[]', '$argon2id$v=19$m=65536,t=4,p=1$YjQyU1VhQS9wRnVidXJOdQ$d8S90C2j4Kd6UBdyY8n+fU20EzT2tjCHXYHJkjHgrfc', 'azmi', 'mch3li', '2022-02-21', 0, '93975384056437fa81aab29b7f1c85d2.jpg'),
+(10, 'azeazeazeaze@gmail.com', '[]', '$argon2id$v=19$m=65536,t=4,p=1$aWJLaDhLL2dreS9VWmJ3Tg$zN+jfEiznmDAezBZWSoFsh9KNSfwphE3UpE4MYEaPpQ', 'Thnin', 'Tletha', '2022-02-21', 0, 'e1e7f4a2ea64bce751398812fa593bbd.png');
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `avis`
@@ -199,9 +295,21 @@ ALTER TABLE `avis`
   ADD UNIQUE KEY `UNIQ_8F91ABF03345E0A3` (`rendezvous_id`);
 
 --
+-- Index pour la table `blog`
+--
+ALTER TABLE `blog`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `carte_fidelite`
 --
 ALTER TABLE `carte_fidelite`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -209,6 +317,14 @@ ALTER TABLE `carte_fidelite`
 --
 ALTER TABLE `commande`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_67F068BC8FABDD9F` (`blog_id_id`),
+  ADD KEY `IDX_67F068BC8F3EC46` (`article_id_id`);
 
 --
 -- Index pour la table `doctrine_migration_versions`
@@ -221,6 +337,14 @@ ALTER TABLE `doctrine_migration_versions`
 --
 ALTER TABLE `livraison`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_DB021E96F624B39D` (`sender_id`),
+  ADD KEY `IDX_DB021E96E92F8F78` (`recipient_id`);
 
 --
 -- Index pour la table `offre`
@@ -261,15 +385,33 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT pour la table `article`
+--
+ALTER TABLE `article`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `avis`
 --
 ALTER TABLE `avis`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT pour la table `blog`
+--
+ALTER TABLE `blog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `carte_fidelite`
 --
 ALTER TABLE `carte_fidelite`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `categorie`
+--
+ALTER TABLE `categorie`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -279,10 +421,22 @@ ALTER TABLE `commande`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `livraison`
 --
 ALTER TABLE `livraison`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT pour la table `offre`
@@ -306,7 +460,7 @@ ALTER TABLE `rendezvous`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Contraintes pour les tables déchargées
@@ -316,7 +470,21 @@ ALTER TABLE `user`
 -- Contraintes pour la table `avis`
 --
 ALTER TABLE `avis`
-  ADD CONSTRAINT `FK_8F91ABF03345E0A3` FOREIGN KEY (`rendezvous_id`) REFERENCES `rendezvous` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_8F91ABF03345E0A3` FOREIGN KEY (`rendezvous_id`) REFERENCES `rendezvous` (`id`);
+
+--
+-- Contraintes pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD CONSTRAINT `FK_67F068BC8F3EC46` FOREIGN KEY (`article_id_id`) REFERENCES `article` (`id`),
+  ADD CONSTRAINT `FK_67F068BC8FABDD9F` FOREIGN KEY (`blog_id_id`) REFERENCES `blog` (`id`);
+
+--
+-- Contraintes pour la table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `FK_DB021E96E92F8F78` FOREIGN KEY (`recipient_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `FK_DB021E96F624B39D` FOREIGN KEY (`sender_id`) REFERENCES `user` (`id`);
 
 --
 -- Contraintes pour la table `offre_produit`
@@ -329,7 +497,7 @@ ALTER TABLE `offre_produit`
 -- Contraintes pour la table `rendezvous`
 --
 ALTER TABLE `rendezvous`
-  ADD CONSTRAINT `FK_C09A9BA8197E709F` FOREIGN KEY (`avis_id`) REFERENCES `avis` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_C09A9BA8197E709F` FOREIGN KEY (`avis_id`) REFERENCES `avis` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
